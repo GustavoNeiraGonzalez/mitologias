0305b97864379654a846f159140f80b590177f00
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2022-08-21 16:50:36 -0400

    agregado estilizacion mas comunicacion con la bdd, planeado que a traves de grid cada fila represente la info de una mitologia (proximamente)

--- client/src/Pages/DeleteHistory/DeleteHistory.jsx
+++ client/src/Pages/DeleteHistory/DeleteHistory.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-export default function deleteHistory() {
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+import style from './DeleteHistory.module.css'
+import style2 from '../NordicaPage/NordicaPage.module.css'
+const DeleteHistory = () => {
+    const [nordica, setNordica] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:3001/api/mitologias/nordica')
+        .then(allNordica =>setNordica(allNordica.data))
+    }, [])
   return (
-    <Container>
-    <Row>
-      <Col>1 of 3</Col>
+    <Container className={style.color+' '+style.resize}>
+    <Row >
+      <Col>
+      {nordica.map(nordic => {
+                            return(
+                                <div key={nordic._id} className={style2.color +' '+style2.paddingpos}>
+                                    <Link to={`/nordica/${nordic._id}`}>
+                                    {nordic.Titulo}
+                                    </Link>     
+                                </div>
+                            )
+                        })}
+      </Col>
       <Col>2 of 3</Col>
       <Col>3 of 3</Col>
     </Row>
   </Container>
   )
 }
+export default DeleteHistory;
