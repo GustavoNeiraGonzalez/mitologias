5719cf3fb06a690aed5e07f61e954af7299d267d
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2022-09-10 01:32:03 -0300

    Solucionado, error de presionar 2 veces el boton para que funcione(modificado el uso del valor por defecto de los inputs traidos desde la bd) RECORDATORIO: no se elimina el login despues de 24 horas de logeado (no sirve el token pero sigue logeado por lo que da un error poco explicito)

--- client/src/Pages/UpdateHistory/UpdateHistory.jsx
+++ client/src/Pages/UpdateHistory/UpdateHistory.jsx
@@ -34,21 +34,27 @@ const UpdateHistory = () => {
       }
     },[])
 
+    
+
     useEffect(() => {
       axios.get(`http://localhost:3001/api/history/${nordica_id}`)
-      .then(nordica => setNordica(nordica.data))
+      .then(Nordica => (setNordica(Nordica.data) ,settitulo(Nordica.data.Titulo)
+      ,setdioses(Nordica.data.Dioses),
+        setfacciones(Nordica.data.Facciones),
+        setpersonajes(Nordica.data.Personajes_importantes),
+        setlugares(Nordica.data.Lugares),
+        sethistoria(Nordica.data.Historia),
+        setfuentes(Nordica.data.Fuentes)))
       .catch(error => console.log(error))
-  }, [])
-
+      
+    }, [])
   const UpdateList = (id) => {
-    settitulo(document.getElementById('Titulo').value)
-    setdioses(document.getElementById('Dioses').value.split(','))
-    setfacciones(document.getElementById('Facciones').value.split(','))
-    setpersonajes(document.getElementById('Personajes_importantes').value.split(','))
-    setlugares(document.getElementById('Lugares').value.split(','))
-    sethistoria(document.getElementById('Historia').value)
-    setfuentes(document.getElementById('Fuentes').value)
-
+    
+    setdioses(Dioses.toString().split(','))
+    setfacciones(Facciones.toString().split(','))
+    setpersonajes(Personajes_importantes.toString().split(','))
+    setlugares(Lugares.toString().split(','))
+    
     axios.put('http://localhost:3001/update' , {
         id:id,
         Titulo:Titulo,
@@ -74,47 +80,75 @@ const UpdateHistory = () => {
       <Form className={style.color}>
         <Form.Group className="mb-3" controlId="Titulo">
           <Form.Label>Titulo</Form.Label>
-          <Form.Control type="text" defaultValue={nordica.Titulo}
+          <Form.Control type="text" onChange={(e) => {
+                settitulo(e.target.value);
+            }}
+
+          defaultValue={Titulo}
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="Dioses">
           <Form.Label>Dioses (array: poner todo junto separado por , )</Form.Label>
-          <Form.Control type="text" defaultValue={nordica.Dioses} 
-          
+          <Form.Control type="text" onChange={(e) => {
+                setdioses(e.target.value);
+            }}
+
+          defaultValue={Dioses}
           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Facciones">
           <Form.Label>Facciones (array: poner todo junto separado por , )</Form.Label>
-          <Form.Control type="text" defaultValue={nordica.Facciones} 
+          <Form.Control type="text" onChange={(e) => {
+                setfacciones(e.target.value);
+            }}
+
+          defaultValue={Facciones}
           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Personajes_importantes">
           <Form.Label>Personajes_importantes (array: poner todo junto separado por , )</Form.Label>
-          <Form.Control type="text" defaultValue={nordica.Personajes_importantes} 
+          <Form.Control type="text" onChange={(e) => {
+                setpersonajes(e.target.value);
+            }}
+
+          defaultValue={Personajes_importantes}
           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Lugares">
           <Form.Label>Lugares (array: poner todo junto separado por , )</Form.Label>
-          <Form.Control type="text" defaultValue={nordica.Lugares} 
+          <Form.Control type="text" onChange={(e) => {
+                setlugares(e.target.value);
+            }}
+
+          defaultValue={Lugares}
           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Historia">
           <Form.Label>Historia</Form.Label>
-          <Form.Control as="textarea" rows={6} defaultValue={nordica.Historia}
-        />
+          <Form.Control as="textarea" rows={6} onChange={(e) => {
+                sethistoria(e.target.value);
+            }}
+
+          defaultValue={Historia}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="Fuentes">
           <Form.Label>Fuentes</Form.Label>
-          <Form.Control type="text" defaultValue={nordica.Fuentes} 
-        />
+          <Form.Control type="text" onChange={(e) => {
+                setfuentes(e.target.value);
+            }}
+
+          defaultValue={Fuentes}
+          />
         </Form.Group>
         <button  id='update' type="button" onClick={()=>{UpdateList(nordica._id)} }>ModifyHistory</button>
         <button type="button" onClick={()=>Delete(nordica._id)}>Delete history</button>
+
       </Form>
       : <Link to="/login">Logeate desgraciao</Link>
     }
